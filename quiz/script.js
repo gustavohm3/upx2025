8f150a33cda7ef2f95b1ddae9b67e682af040e9c
Gustavo Henrique <126521748+gustavohm3@users.noreply.github.com>
2025-06-04 17:56:02 -0300

    Add files via upload

---
+++ quiz/script.js
@@ -0,0 +1,62 @@
+const quiz = [
+    {
+        q: 'Qual dessas ações contribui mais para a economia de energia em uma residência?',
+        options:['Pintar as paredes de cores escuras', 'Usar lâmpadas incandescentes',
+            'Aproveitar a luz natural durante o dia', 'Deixar o carregador do celular sempre na tomada'],
+        answer: '2'
+    }, 
+    {
+        q: 'Qual é a principal vantagem das lâmpadas de LED?',
+        options:['São mais baratas que as incandescentes', 'Consomem menos energia e duram mais',
+            'Aumentam o calor do ambiente', 'Podem ser ligadas sem interruptor'],
+        answer: '1'
+    },
+    {
+        q: 'Qual hábito gasta mais energia desnecessariamente?',
+        options:['Usar ventilador à noite', 'Deixar luzes acesas em ambientes vazios',
+            'Lavar roupas uma vez por semana', 'Usar micro-ondas para esquentar comida'],
+        answer: '1'
+    },
+    {
+        q: 'O que significa a etiqueta ENCE do Inmetro nos eletrodomésticos?',
+        options:['É uma taxa ambiental', 'Indica o valor da conta de energia',
+            'Mostra a eficiência energética do aparelho', 'Avalia a segurança elétrica'],
+        answer: '2'
+    },
+    {
+        q: 'Qual aparelho costuma ser um dos vilões da conta de luz?',
+        options:['Televisor de LED', 'Geladeira antiga',
+            'Liquidificador', 'Ferro de passar'],
+        answer: '1'
+    },
+    {
+        q: 'Qual é a forma mais eficiente de ventilar um ambiente?',
+        options:['Usar ventilador virado para a parede', 'Abrir janelas opostas (ventilação cruzada)',
+            'Usar ar-condicionado o dia todo', 'Deixar o ambiente fechado e escuro'],
+        answer: '1'
+    },
+    {
+        q: 'Como economizar energia ao lavar roupas?',
+        options:['Usar a máquina com pouca roupa', 'Usar água quente sempre',
+            ' Juntar roupas e lavar em ciclos completos', 'Lavar cada peça separadamente'],
+        answer: '2'
+    },
+    {
+        q: 'Qual fonte de energia é considerada limpa e renovável?',
+        options:['Carvão mineral', 'Petróleo',
+            'Energia solar', 'Gás natural'],
+        answer: '2'
+    },
+    {
+        q: 'Qual a função do chuveiro na conta de energia?',
+        options:['Não interfere', 'É um dos maiores consumidores',
+            'Gasta menos que uma TV', 'Gasta só se for usado com água fria'],
+        answer: '1'
+    },
+    {
+        q: 'O que significa eficiência energética?',
+        options:['Usar mais energia para melhores resultados', 'Reduzir o uso de energia, sem perder desempenho',
+            'Pagar menos na conta de luz', 'Usar apenas energia solar'],
+        answer: '1'
+    }
+]
